be7159c98e0d2e0b35b31aae2da56600b06e87a7
Jadie <jaden.tiesler@gmail.com>
2021-02-08 11:15:38 +0100

    add random_string function

--- UsefulFunctions/UsefulFunctions/Class1.cs
+++ UsefulFunctions/UsefulFunctions/Class1.cs
@@ -127,5 +127,18 @@ namespace UsefulFunctions
             string abc = replace_strings(abca, abcb, original);
             return abc;
         }
+        string random_string(int length)
+        {
+            string random = "";
+            Random r = new Random();
+            string[] a = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "\"", "§", "$", "%", "&", "/", "(", ")", "=", "?", "²", "³", "{", "[", "]", "}", @"\", "^", "°", "@", "<", ">", "|", ",", ";", ".", ":", "-", "_", "#", "'", "*", "+", "~", "´", "`", "€", "µ", "¥", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+            string b = "";
+            for (int i = 0;i<length;i++)
+            {
+                b = b + r.Next(0, a.Length);
+            }
+            random = b;
+            return random;
+        }
     }
 }
